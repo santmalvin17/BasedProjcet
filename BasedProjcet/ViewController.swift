2ecc705ae6b15a06a0c24f51ca8ea1e3268f90a0
santmalvin17 <malvinsantoso023@gmail.com>
2020-07-01 13:19:47 +0700

    commit make based
    commit make

---
+++ BasedProjcet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BasedProjcet
+//
+//  Created by Malvin Santoso on 01/07/20.
+//  Copyright © 2020 Malvin Santoso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
