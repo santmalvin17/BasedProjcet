da5db2c9b22dafae2e8400027b82976773907545
Gregory Kevin <gregorykevin1211@gmail.com>
2020-07-02 00:22:42 +0700

    AppDelegate navbar lagiiii

--- BasedProjcet/Class/Chat/ChatViewController.swift
+++ BasedProjcet/Class/Chat/ChatViewController.swift
@@ -2,7 +2,7 @@
 //  ChatViewController.swift
 //  BasedProjcet
 //
-//  Created by Gregory Kevin on 01/07/20.
+//  Created by Gregory Kevin on 02/07/20.
 //  Copyright © 2020 Malvin Santoso. All rights reserved.
 //
 
